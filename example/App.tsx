51c6a143e44ab68d004c5df7567f227bcd197270
Ena Fleurence <ena@csa360software.com>
2024-01-31 22:47:53 +0800

    Fix the error in Brother Module

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import * as BrotherPrint from 'brother-print';
+import * as BrotherPrint from "brother-print";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [printers, setPrinters] = useState<any[]>([]);
+  const [printerMessage, setPrinterMessage] = useState("");
+
+  const onFindPrinterViaWifi = async () => {
+    const [printersArray] = await BrotherPrint.startSearchWiFiPrinter();
+    if (Array.isArray(printersArray)) {
+      setPrinters(printersArray);
+    } else {
+      setPrinterMessage(printersArray);
+    }
+  };
+
+  const displayPrinters = () => {
+    if (Array.isArray(printers)) {
+      return printers.map((printer, index) => {
+        return <Text> {printer.modelName} </Text>;
+      });
+    } else {
+      return printers;
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>{BrotherPrint.startSearchWiFiPrinter()}</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          onPress={() => onFindPrinterViaWifi()}
+          title="Find Printers via Wifi"
+          color="white"
+        />
+      </View>
+      <Text>{displayPrinters()}</Text>
+      <Text>{printerMessage} </Text>;
     </View>
   );
 }
@@ -13,8 +42,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    margin: 20,
+    backgroundColor: "#3796f4",
+    padding: 10,
+    borderRadius: 10,
   },
 });
--- ios/BrotherPrintModule.swift
+++ ios/BrotherPrintModule.swift
@@ -1,21 +1,22 @@
 import ExpoModulesCore
+
 import BRLMPrinterKit
 
 public class BrotherPrintModule: Module {
   public func definition() -> ModuleDefinition {
     Name("BrotherPrint")
 
-    Function("startSearchWiFiPrinter") { () -> Void in
-      let manager = BRPtouchNetworkManager();
-      manager.startSearch(5);
-      let devices = manager.getPrinterNetInfo()  as! [BRPtouchDeviceInfo];
-      var printers = "";
-      for deviceInfo in devices {
-          if let deviceInfo = deviceInfo as? BRPtouchDeviceInfo {
-              printers += deviceInfo.strModelName + " ";
-          }
+    AsyncFunction("startSearchWiFiPrinter") { (promise: Promise) in
+        let manager = BRPtouchNetworkManager();
+        manager.startSearch(5);
+        let devices = manager.getPrinterNetInfo()  as! [BRPtouchDeviceInfo];
+        var printers = "";
+        for deviceInfo in devices {
+            if let deviceInfo = deviceInfo as? BRPtouchDeviceInfo {
+                printers += deviceInfo.strModelName + " ";
+            }
+        }
+        promise.resolve( devices as! [BRPtouchDeviceInfo]);
       }
-      return devices as! [BRPtouchDeviceInfo];
-    }
   }
 }
